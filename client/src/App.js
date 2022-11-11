10a3c8bbc7ca9085e838a3319a8085a18841cdbc
Hakeem <hakeemdurogbola@gmail.com>
2022-11-11 22:33:34 +0100

    created a react-app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import { useQuery } from "@apollo/client"
+import './App.css';
+import DisplayData from "./components/DisplayData"
+
+function App() {
+  
+  return (
+    <div className="App">
+      <DisplayData />
+    </div>
+  );
+}
+
+export default App;
