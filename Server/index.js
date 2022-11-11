bbde72dc80dfc106984e9e3a90f3e70fe7edd6d0
Hakeem <hakeemdurogbola@gmail.com>
2022-11-11 16:45:43 +0100

    query data in the fakeDatabase

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,6 @@
 const { ApolloServer } = require("apollo-server");
 const { typeDefs } = require("./schema/type_defs");
-const { resolvers } = require("./schema/resolvers")
+const { resolvers } = require("./schema/resolvers");
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
--- Server/schema/resolvers.js
+++ Server/schema/resolvers.js
@@ -1,8 +1,36 @@
-const { UserList } = require("../db");
+const { UserList, MovieList } = require("../fakeData");
+const _ = require("lodash");
 const resolvers = {
   Query: {
-    users() {
+    // all users
+    users: () => {
       return UserList;
+    },
+    // single user
+    user: (parent, args) => {
+      const id = args.id;
+      const user = _.find(UserList, { id: Number(id) });
+      return user;
+    },
+        // all movies
+    movies: () => {
+      return MovieList;
+    },
+    // single movie
+    movie: (parent, args) => {
+      const name = args.name;
+      const movie = _.find(MovieList, { name });
+      return movie;
+    },
+  },
+  //favoriteMovies
+  User: {
+    favoriteMovies: () => {
+      return _.filter(
+        MovieList,
+        (movie) =>
+          movie.yearOfPublication > +2000 && movie.yearOfPublication <= 2010
+      );
     },
   },
 };
